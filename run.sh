fd34b6f8e510a4a5bb9bcbe5d3c19811cb4d2cec
Simon Schwarz <simonschwarz.du@gmail.com>
2024-11-01 11:29:19 +0100

    issue test

--- run.sh
+++ run.sh
@@ -1,7 +1,1 @@
-TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-
-curl http://169.254.169.254/
-
-echo "ehlo"
-
-curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/
+echo "Ehlo on push"
